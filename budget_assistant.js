cef07dd5c76f084cc793c0518b4c0b8bd8e35eea
Mikayla Michael <memichael@usf.edu>
2025-06-01 18:19:43 -0400

    Create and commit budget_assistant.js

---
+++ budget_assistant.js
@@ -0,0 +1,16 @@
+
+let startupnname = "FirstTech";
+let initialCapital = 10000;
+let monthly Revenue = 7500;
+let monthlyExpenses = 5000:
+
+
+let netCashFlow = monthlyRevenue - monthlyExpenses;
+let currentBalance =  initialCapital + netCashFlow;
+let isProfitablle + netCashFlow > 0;
+
+
+console.log("Startup Name: " + startupnname);
+console.log("Net Monthly Cash Flow: $" + netCashFlow);
+console.log("Current Balance: $" + currentBalance);
+console.log("Is the startup profitable? " + (isProfitablle ? "Yes" : "No"));
